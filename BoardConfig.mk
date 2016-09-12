899f3a50baa964f569c80799336f83bf658f07c2
electrikjesus <electrikjesus@gmail.com>
2016-09-11 21:39:49 -0400

    Add vendor symlink requirement back

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -118,6 +118,10 @@ BOARD_CACHEIMAGE_PARTITION_SIZE := 100663296
 BOARD_CACHEIMAGE_FILE_SYSTEM_TYPE := ext4
 BOARD_FLASH_BLOCK_SIZE := 131072
 
+BOARD_NEEDS_VENDOR_SYMLINK:=true
+
+-include vendor/lge/bullhead/BoardConfigVendor.mk
+
 # Build a separate vendor.img
 TARGET_COPY_OUT_VENDOR := vendor
 
@@ -150,4 +154,3 @@ TARGET_PER_MGR_ENABLED := true
 
 USE_CLANG_PLATFORM_BUILD := true
 
--include vendor/lge/bullhead/BoardConfigVendor.mk
